90acfcb97d1a23466a7c88f8a6b365ef07e2c3f5
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-04-19 14:00:34 -0500

    Finished it

--- cards.js
+++ cards.js
@@ -1,57 +1,34 @@
 // When the user enters in text into the text area and then clicks the create button, create a new card element in the DOM that includes it's own delete button. You decide the height/width of the card.
 
+// When the user clicks the Delete button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
+
 var cardContent = document.getElementById("cardContent");
 var button = document.getElementById("create");
 var output = document.getElementById("addCards");
 var deleteButton = document.getElementsByClassName("delete");
-var addID = document.getElementsByClassName("addId");
 var cardNumber = document.getElementsByClassName("card");
+var sectionID = document.getElementsByTagName("section");
 
-// button.addEventListener("click", function(event){
-// 	var cardStructure = `<div class="card">
-// 							<h2>${cardContent.value}</h2>
-// 							<button class="delete">Delete</button>
-// 						</div>`;
-// 	var cardDiv = document.createElement("div");
-// 	cardDiv.innerHTML = cardStructure;
-// 	output.appendChild(cardDiv);
-	
-// 	console.log("deleteButton", deleteButton);
-	
-// 	for (var i = 0; i < deleteButton.length; i++){
-// 		var specificButton = deleteButton.item(i);
-// 		specificButton.addEventListener("click", function(event){
-// 			console.log(`You pushed the delete button!`);
-// 		});
-// 	}
-// });
-
-
-
-// When the user clicks the Delete button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
-
-button.addEventListener("click", function(event){
+function makeNewCards(){
 	var cardStructure = `<div class="card">
 							<h2>${cardContent.value}</h2>
-							<button class="addId">Delete</button>
+							<button class="delete">Delete</button>
 						</div>`;
-	var cardDiv = document.createElement("div");
-	cardDiv.innerHTML = cardStructure;
-	output.appendChild(cardDiv);
-	
-	console.log("deleteButton", deleteButton);
-	for (var i = 0; i < addID.length; i++){
-		addID.item(i).setAttribute("id", `delete${i}`);
-		cardNumber.item(i).setAttribute("id", `card${i}`)
-	};
+	var cardSection = document.createElement("section");
+	cardSection.innerHTML = cardStructure;
+	output.appendChild(cardSection);
+}
 
-	for (var i = 0; i < addID.length; i++){
-	var deleteButton = document.getElementById(`delete${i}`);
-	deleteButton.addEventListener("click", function(event){
-		var top = document.getElementById(`card${i}`);
-		var nested = document.getElementById(`delete${i}`);
-		top.removeChild(nested);
-		});
-	}
+
+button.addEventListener("click", function(event){
+	makeNewCards();
+	for (var i = 0; i < deleteButton.length; i++){
+		deleteButton.item(i).addEventListener("click", function(event){
+			var getParent = event.target.parentNode;
+			var getGParent = getParent.parentNode;
+			console.log("getParent", getParent);
+			getGParent.removeChild(getParent);
+		})
+	};
 });
 
